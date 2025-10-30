d08723493a5b04d8e9eb2603cefa73403f537d6a
Merk42 <mark_c@markecurtis.com>
2025-10-30 13:27:35 -0400

    lesson layouts

--- app/components/Lesson.vue
+++ app/components/Lesson.vue
@@ -15,14 +15,14 @@ const totaltime = computed(() => {
         <div class="grid gap-1 p-1 @[50vw]:grid-cols-[auto_1fr_auto] border-1 border-solid rounded-sm">
             <img alt="img name" :src="lesson.Image" class="rounded-sm overflow-hidden w-full @[50vw]:h-[50px]"/>
             <span class="itemTitle" v-html="lesson.Name"></span>
-            <span class="flex gap-1 items-center">
-                <UProgress class="@[50vw]:col-span-3 @[50vw]:order-1"  v-model="lesson.progress" :max="lesson.duration" />
+            <span class="flex gap-1 items-center  @[50vw]:order-1 @[50vw]:col-span-3">
+                <UProgress class="@[50vw]:col-span-3"  v-model="lesson.progress" :max="lesson.duration" />
                 <span class="hidden md:block">{{ totaltime }}</span>
             </span>
             
-            <span class="text-right">
+            <span class="text-right flex gap-2">
                 <UIcon :name="lesson.bookmarked ? 'fa7-solid:bookmark' : 'fa7-regular:bookmark'" class="size-5" />
-                <UIcon :name="lesson.progress >= lesson.duration ? 'fa7-solid:play-circle' : 'fa7-regular:play-circle'" class="size-5" />
+                <UIcon :name="lesson.progress >= lesson.duration ? 'fa7-solid:check-circle' : 'fa7-regular:check-circle'" class="size-5" />
             </span>
         </div>
     </div>
