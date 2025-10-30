173df6f96cc44f6053c521ebbf47e643aeb57e07
Merk42 <mark_c@markecurtis.com>
2025-10-30 13:09:43 -0400

    UForm updates

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,34 +1,48 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-const username = ref("");
-const password = ref("")
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
 
-const cancontinue = computed(() => {
-    return !!username.value && !!password.value
+const schema = z.object({
+  username:  z.string().min(1, 'Must be at least 1 character'),
+  password: z.string().min(8, 'Must be at least 8 characters')
 })
 
-const handleSubmit = async () => {
+type Schema = z.output<typeof schema>
+
+const state = reactive<Partial<Schema>>({
+  username: undefined,
+  password: undefined
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+    console.log('onsubmit fired...')
     navigateTo('/overview')
 }
+
+const cancontinue = computed(() => {
+    return !!state.username && !!state.password
+})
+
 </script>
 <template>
-    <div id="sign-in-page" class="md:grid md:grid-cols-[400px_1fr]">
-        <div id="effect" class="hidden md:block p-4">
-            <h1>Welcome Back</h1>
+    <div id="sign-in-page" class="md:grid md:grid-cols-[400px_1fr] h-screen">
+        <div id="effect" class="hidden md:block p-4 bg-blue-600">
+            <h1 class="text-5xl text-white">Welcome Back</h1>
         </div>
         <div id="form-cell" class="flex justify-center">
-            <div id="form-container">
-                <h2>Sign in to ArtistWorks</h2>
+            <div id="form-container" class="self-center">
+                <h2 class="text-3xl">Sign in to ArtistWorks</h2>
                 <p>New user? <a href="#">Create an account.</a></p>
-                <form @submit.prevent="handleSubmit">
-                    <UFormField label="Enter username">
-                        <UInput v-model="username" />
+                <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+                    <UFormField label="Enter username" size="xl">
+                        <UInput v-model="state.username" />
                     </UFormField>
-                    <UFormField label="Enter password">
-                        <UInput v-model="password" />
+                    <UFormField label="Enter password" size="xl">
+                        <UInput v-model="state.password" type="password"/>
                     </UFormField>
-                    <UButton :disabled="!cancontinue">Continue</UButton>
-                </form>
+                    <UButton type="submit" size="xl" class="w-full rounded-full text-center justify-center">Continue</UButton>
+                </UForm>
                 <p><a href="#">Forgot password?</a></p>
 
                 <span class="hr-span">Or sign up with</span>
